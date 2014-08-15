15e7c3b1917b71436bb99082bcba5bd90ed09314
sergiomcr21 <SergioMCR@MacBook-Pro-de-SERGIO.local>
2014-08-15 11:25:27 -0600

    Commit Inicial 20140815_1125

--- CursoTEC_1/main.m
+++ CursoTEC_1/main.m
@@ -15,6 +15,69 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         NSLog(@"Hello, World!");
+        NSString *mensaje = @"Hola a la Calculadora";
+        NSLog(@"%@", mensaje);
+        
+        float primerValor;
+        scanf("%f", &primerValor);
+        
+        int opcion;
+        scanf("%d", &opcion);
+        
+        float segundoValor;
+        scanf("%f", &segundoValor);
+        
+        float operacion = 0;
+        // 0 sumar, 1 restar, 2 multiplicar, 3 dividir
+        switch (opcion) {
+            case 0:
+                operacion = primerValor + segundoValor;
+                break;
+            case 1:
+                operacion = primerValor - segundoValor;
+                break;
+            case 2:
+                operacion = primerValor * segundoValor;
+                break;
+            case 3:
+                operacion = primerValor / segundoValor;
+                break;
+            default:
+                NSLog(@"Opción no valida!!!");
+                break;
+        }
+        
+        if(operacion < 100){
+            NSLog(@"mensaje 1");
+        }else{
+            NSLog(@"mensaje 2");
+        }
+        
+        NSMutableArray *arrayOperaciones = [[NSMutableArray alloc]init];
+ 
+        NSMutableArray *array;
+        array = [NSMutableArray alloc];
+        array = [NSMutableArray init];
+    
+        NSNumber *numero;
+        numero = [NSNumber alloc];
+        numero = [[NSNumber alloc] initWithFloat:operacion];
+
+        [array addObject:numero];
+        [array count];
+        
+        NSDictionary *dictionary = @{
+                                     @"nombre":@"sergio",
+                                     @"edad":@"22",
+                                     @"lenguaje":@"objetive-c"
+                                     };
+        
+        NSMutableDictionary *dic = [[NSMutableDictionary alloc] init];
+        [dic setValue:@"carreon" forKey:@"apellido"];
+        
+        [arrayOperaciones addObject:dic];
+        [arrayOperaciones addObject:dictionary];
+        
         
     }
     return 0;
