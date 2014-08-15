036d307cc700eb65adb724c1b2008cf97ccb6a48
sergiomcr21 <SergioMCR@MacBook-Pro-de-SERGIO.local>
2014-08-15 10:09:39 -0600

    Initial Commit

---
+++ CursoTEC_1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CursoTEC_1
+//
+//  Created by SERGIO MARIO CARREÓN ROMÁN on 15/08/14.
+//  Copyright (c) 2014 SergioMCR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
